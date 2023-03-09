6bf225a46c83b55f590471fbb79902d840ad9dba
zayan <mohammadzayan444@gmail.com>
2023-03-09 14:38:10 +0530

    flowmo timer ui

--- src/components/Flomo.vue
+++ src/components/Flomo.vue
@@ -1,5 +1,26 @@
 <template>
     <div class="transparent flowmo">
-        
+        <h2>Title</h2>
+        <h1>00:00</h1>
+        <div class="btn-con">
+            <button>Start</button>
+            <button disabled>Reset</button>
+        </div>
     </div>
 </template>
+<style scoped>
+.flowmo{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 16px;
+}
+.btn-con{
+    margin-top: 8px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap:8px;
+}
+</style>
