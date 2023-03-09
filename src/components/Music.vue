ac4a26e1db9e07b8d406e239b3aa59cc139cd578
zayan <mohammadzayan444@gmail.com>
2023-03-09 17:36:20 +0530

    spotify added

---
+++ src/components/Music.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="transparent music">
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/4eWBwGl0c5wtp6k5Krp6My?utm_source=generator&theme=0"  frameBorder="0" allowfullscreen="false" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+</template>
+<style scoped>
+.music{
+    width: 250px;
+    height: 80px;
+}
+.music iframe{
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -2,11 +2,13 @@
 import BackgroundVideo from '../components/BackgroundVideo.vue';
 import TaskBar from '../components/TaskBar.vue';
 import Flomo from "../components/Flomo.vue"
+import Music from '../components/Music.vue';
 </script>
 <template>
     <div class="homepage">
         <BackgroundVideo />
         <TaskBar />
         <Flomo />
+        <Music />
     </div>
 </template>
