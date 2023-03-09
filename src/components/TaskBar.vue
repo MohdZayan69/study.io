2383c42cc01affebc4de1c48b0c62fb9ab4865f2
zayan <mohammadzayan444@gmail.com>
2023-03-09 14:03:14 +0530

    taskbar style done

---
+++ src/components/TaskBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="taskBar transparent"></div>
+  </div>
+</template>
+<style scoped>
+.container{
+    width: 250px;
+    height: 40px;
+    position: absolute;
+    bottom: 0;
+    left:50%;
+    transform: translate(-50%, 0%);
+}
+.container:hover > .taskBar{
+    transform: translate(0%, 0);
+}
+</style>
--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import BackgroundVideo from '../components/BackgroundVideo.vue';
+import TaskBar from '../components/TaskBar.vue';
 </script>
 <template>
     <div>
         <BackgroundVideo />
+        <TaskBar />
     </div>
 </template>
