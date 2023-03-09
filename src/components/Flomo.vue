d10e5a91598325cb046879abe0fcad89c2e0a1c5
zayan <mohammadzayan444@gmail.com>
2023-03-09 14:07:17 +0530

    flomo styles

---
+++ src/components/Flomo.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="transparent flowmo">
+        
+    </div>
+</template>
--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import BackgroundVideo from '../components/BackgroundVideo.vue';
 import TaskBar from '../components/TaskBar.vue';
+import Flomo from "../components/Flomo.vue"
 </script>
 <template>
-    <div>
+    <div class="homepage">
         <BackgroundVideo />
         <TaskBar />
+        <Flomo />
     </div>
 </template>
