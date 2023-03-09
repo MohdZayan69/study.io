23bfe4e8322c180bba82f2f6d655060dee38b7dd
zayan <mohammadzayan444@gmail.com>
2023-03-09 13:48:30 +0530

    bgVideo done

---
+++ src/components/BackgroundVideo.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import bgVideo from "../assets/purple-lofi-bedroom-moewalls.com.mp4"
+</script>
+<template>
+    <div class="bgVideo">
+        <video autoplay muted loop :src="bgVideo">
+        </video>
+    </div>
+</template>
---
+++ src/views/HomeScreen.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import BackgroundVideo from '../components/BackgroundVideo.vue';
+</script>
+<template>
+    <div>
+        <BackgroundVideo />
+    </div>
+</template>
