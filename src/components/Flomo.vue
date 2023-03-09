c0459a8eac74eb255556b02ebb0d7bad637b1df1
zayan <mohammadzayan444@gmail.com>
2023-03-09 15:00:58 +0530

    cleaner toggle

--- src/components/Flomo.vue
+++ src/components/Flomo.vue
@@ -1,45 +1,42 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-enum CommandText{
-    start = "Start",
-    pause = "Pause",
+import { ref } from "vue";
+enum CommandText {
+  start = "Start",
+  pause = "Pause",
 }
-let commandText = ref(CommandText.start)
-let isTimerActive = ref(true)
-const handleCommand = ()=>{
-    if(commandText.value === CommandText.start) {
-        commandText.value = CommandText.pause
-        isTimerActive.value = false
-    }
-    else if(commandText.value === CommandText.pause) {
-        commandText.value = CommandText.start
-        isTimerActive.value = true
-    }
-}
+let commandText = ref(CommandText.start);
+let isTimerActive = ref<boolean>(true);
+let focusMinutes = ref<number>(0);
+const handleCommand = () => {
+    if(commandText.value !== CommandText.start) commandText.value = CommandText.start
+    else if(commandText.value !== CommandText.pause) commandText.value = CommandText.pause
+    if(commandText.value == CommandText.start) isTimerActive.value = true
+    else if(commandText.value == CommandText.pause) isTimerActive.value = false
+};
 </script>
 <template>
-    <div class="transparent flowmo">
-        <h2>Title</h2>
-        <h1>00:00</h1>
-        <div class="btn-con">
-            <button @click="handleCommand">{{ commandText }}</button>
-            <button :disabled="isTimerActive">Reset</button>
-        </div>
+  <div class="transparent flowmo">
+    <h2>Title</h2>
+    <h1>00:00</h1>
+    <div class="btn-con">
+      <button @click="handleCommand">{{ commandText }}</button>
+      <button :disabled="isTimerActive">Reset</button>
     </div>
+  </div>
 </template>
 <style scoped>
-.flowmo{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 16px;
+.flowmo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 16px;
 }
-.btn-con{
-    margin-top: 8px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap:8px;
+.btn-con {
+  margin-top: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
 }
 </style>
